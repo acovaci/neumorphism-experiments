9e9aaa183697a346aaa73404ba919c1abb17dd73
Adrian Covaci <6562353+acovaci@users.noreply.github.com>
2023-05-01 18:07:38 +0100

    Basic page structure

---
+++ src/components/Page.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Page = styled.div`
+    margin: 0 auto;
+    max-width: 720px;
+    padding: 0 1rem;
+`;
---
+++ src/pages/SolidComponentPage.tsx
@@ -0,0 +1,7 @@
+import { Page } from "../components/Page";
+
+export const SolidComponentPage = () => (
+    <Page>
+        <h1>Solid</h1>
+    </Page>
+);
